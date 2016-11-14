3ff10e4d41d03d34aaff0290df2b94377c2660bc
J1aDong <991383877@qq.com>
2016-11-14 14:46:35 +0800

    主页添加tableView

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 [NSUInteger和NSInteger和int](http://www.jianshu.com/p/c883674b6de5)
 
 ## 使用到的的第三方库
-[SnapKit 第三方布局框架](https://github.com/SnapKit/SnapKit)
+* [SnapKit 第三方布局框架](https://github.com/SnapKit/SnapKit)
+* [UIButton的使用(swift3)](http://www.hangge.com/blog/cache/detail_529.html)
--- iOS-StepByStep/ViewController.swift
+++ iOS-StepByStep/ViewController.swift
@@ -7,13 +7,39 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var items = ["武汉","上海","北京","深圳","广州","重庆","香港","台海","天津"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        self.view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            (make) -> Void in
+            make.edges.equalTo(self.view).inset(UIEdgeInsetsMake(20, 0, 0, 0))
+        }
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = self.items[indexPath.row]
+        return cell
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
